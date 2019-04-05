a02bcd7a6559092e016336cf341b980ea480eba8
Juan Carlos Pelaez <jucape7@gmail.com>
2019-04-05 10:34:15 -0500

    Se creo el contador y se creo el archivo para añadir los datos del contador

--- index.js
+++ index.js
@@ -2,17 +2,42 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var fs = require('fs');
+
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/home.html'))
-app.get('/contacto', function(req, res){
-    res.send('contacto');
+var contador = {
+    home: 0,
+    contacto: 0,
+};
+
+
+
+//app.get('/', (req, res) => res.sendFile(__dirname + '/public/home.html'))
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/home.html');
+    contador.home++;
+    console.log('pagina vista: ', contador);
+
+    let contenido = 'home:' + contador.home + '\ncontacto: ' + contador.contacto;
+    fs.writeFile('contador.txt', contenido, 'utf8', function () {
+        console.log('archivo escrito');
+    });
 });
-app.get('/sobre-mi',function(req,res){
-   res.json({
-       name:'Juan',
-       age:27
-   })
+app.get('/contacto', function (req, res) {
+    res.send('contacto');
+    contador.contacto++;
+
+    let contenido = 'home:' + contador.home + '\ncontacto: ' + contador.contacto;
+    fs.writeFile('contador.txt', contenido, 'utf8', function () {
+        console.log('archivo escrito');
+    });
+});
+app.get('/sobre-mi', function (req, res) {
+    res.json({
+        name: 'Juan',
+        age: 27
+    })
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
